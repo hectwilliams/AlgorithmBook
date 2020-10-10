a7aa16e88411f6c9f528caaa051fe6ca4913c78f
hectwilliams <hectwilliams@gmail.com>
2020-10-09 21:25:13 -0700

    avl tests

--- chapter19/c/avl_tests.h
+++ chapter19/c/avl_tests.h
@@ -10,23 +10,32 @@
 
 //  remove node with 1 child
 
-void avl_remove_single_child_node_a ();  // avl_remove_balanced/index.html  slide 5
-void avl_remove_single_child_node_b ();  // avl_remove_balanced/index.html   slide 6
-void avl_remove_single_child_node_c ();  // avl_remove_balanced/index.html    slide 7
+void avl_remove_single_child_node_a ();  // avl_remove.pdf  slide 5
+void avl_remove_single_child_node_b ();  // avl_remove.pdf   slide 6
+void avl_remove_single_child_node_c ();  // avl_remove.pdf    slide 7
 
 //  remove node with 0 children
-void avl_remove_zero_children_node_a ();  // avl_remove_balanced/index.html  slide 19
-void avl_remove_zero_children_node_b ();  // avl_remove_balanced/index.html  slide 20
-void avl_remove_zero_children_node_c ();  // avl_remove_balanced/index.html  slide 21
+void avl_remove_zero_children_node_a ();  // avl_remove.pdf  slide 19
+void avl_remove_zero_children_node_b ();  // avl_remove.pdf  slide 20
+void avl_remove_zero_children_node_c ();  // avl_remove.pdf  slide 21
 
 // remove node with 2 children
-void avl_remove_full_node_a (); // avl_remove_balanced/index.html slide 14
-void avl_remove_full_node_b (); // avl_remove_balanced/index.html slide 15
-void avl_remove_full_node_c (); // avl_remove_balanced/index.html slide 16
-void avl_remove_full_node_d (); // avl_remove_balanced/index.html slide 17
-void avl_remove_full_node_e (); // avl_remove_balanced/index.html slide 18
-void avl_remove_full_node_f (); // avl_remove_balanced/index.html slide 19
+void avl_remove_full_node_a (); // avl_remove.pdf  slide 14
+void avl_remove_full_node_b (); // avl_remove.pdf  slide 15
+void avl_remove_full_node_c (); // avl_remove.pdf  slide 16
+void avl_remove_full_node_d (); // avl_remove.pdf  slide 17
+void avl_remove_full_node_e (); // avl_remove.pdf  slide 18
+void avl_remove_full_node_f (); // avl_remove.pdf  slide 19
 
+
+/* AVL Add balanced */
+void balanced_add_test ();  // reference avl_balanced_add_remove.pdf
+
+/* AVL Add balanced */
+void balanced_remove_test ();  // reference avl_balanced_add_remove.pdf
+
+/* AVL Repair test */
+void repair_test ();  // reference avl_balanced_add_remove.pdf
 
 
 void avl_remove_single_child_node_a ()
@@ -287,7 +296,6 @@ void avl_remove_full_node_d ()
   AVLTree_add(&tree, 140);
   AVLTree_add(&tree, 110);
 
-
   display(tree);
   printf("\n");
   AVLTree_remove(&tree, 100);
@@ -350,3 +358,64 @@ void avl_remove_full_node_e ()
 */
 }
 
+
+void balanced_add_test ()
+{
+  struct AVLTree *tree = NULL;
+
+  AVLTree_balanced_add(&tree, 100);
+  AVLTree_balanced_add(&tree, 50 );
+  AVLTree_balanced_add(&tree, 200);
+  AVLTree_balanced_add(&tree, 300);
+  AVLTree_balanced_add(&tree, 400);
+  AVLTree_balanced_add(&tree, 25);
+  AVLTree_balanced_add(&tree, 5 );
+  AVLTree_balanced_add(&tree, 10);
+  display(tree);
+
+}
+
+void balanced_remove_test ()
+{
+  struct AVLTree *tree = NULL;
+
+  AVLTree_balanced_add(&tree, 100);
+  AVLTree_balanced_add(&tree, 50 );
+  AVLTree_balanced_add(&tree, 200);
+  AVLTree_balanced_add(&tree, 300);
+  AVLTree_balanced_add(&tree, 400);
+  AVLTree_balanced_add(&tree, 25);
+  AVLTree_balanced_add(&tree, 5 );
+  AVLTree_balanced_add(&tree, 10);
+  AVLTree_balanced_remove(&tree, 10);
+  display(tree);
+  printf("\n");
+  AVLTree_balanced_remove(&tree, 25);
+  display(tree);
+  printf("\n");
+  AVLTree_balanced_remove(&tree, 5);
+  display(tree);
+  printf("\n");
+  AVLTree_balanced_remove(&tree, 50);
+  display(tree);
+  printf("\n");
+
+}
+
+
+void repair_test ()
+{
+  struct AVLTree *tree = NULL;
+  AVLTree_add(&tree, 100);
+  AVLTree_add(&tree, 50);
+  AVLTree_add(&tree, 200);
+  AVLTree_add(&tree, 500);
+  AVLTree_add(&tree, 250);
+  AVLTree_add(&tree, 700);
+  AVLTree_add(&tree, 220);
+  AVLTree_add(&tree, 350);
+  AVLTree_add(&tree, 270);
+  AVLTree_repair(&tree);
+  display(tree);
+  printf("\n");
+}
