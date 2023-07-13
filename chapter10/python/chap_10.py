a8b996c3b22559f184b8e1e5e705c2288b791ed0
Hector Williams <hectorwilliams@Hectors-Air.lan>
2023-07-13 17:52:50 -0400

    removed old sol set

--- chapter10/python/chap_10.py
+++ chapter10/python/chap_10.py
@@ -64,8 +64,6 @@ def longest_word(msg):
     
     def valid_node (node):
         
-        print(node.word, 'DEBUG') # graph of words 
-
         if node.char_found :
             if node.pos == len(msg) : 
                 return True  
@@ -367,6 +365,7 @@ def perm_arr(array):
             queue.append(new_node)
     return buffer 
 
+# NOTE : TBD :( 
 def optimal_sequence(arr):
     class seq_node:
         def __init__(self, arr= []): 
@@ -750,7 +749,6 @@ def num_to_text(num):
     # allocate table memory 
     for _ in range(l):
         table.append( [] )
-    print(table, len(table))
 
     # load table 
     while index > -1 :
@@ -763,6 +761,7 @@ def num_to_text(num):
             col_count == 0
             row_count = ( row_count + 1)  %3
 
+    # output string 
     s_whole = ""
     
     for i, row_data  in enumerate(table):
@@ -774,6 +773,420 @@ def num_to_text(num):
     
     return s_whole + "" + s_dec
 
-
 number = 222111.2051
 print('{}'.format( num_to_text(number)) )
+
+def is_permutation(s1, s2 = ""):
+    class perm_node:
+        def __init__(self, raw_string = ""):
+            self.s = ""
+            self.prev = [] 
+            self.chars = raw_string
+
+    def clone(parent):
+        c_node = perm_node()
+        c_node.s = parent.s
+        c_node.prev = parent.prev + []
+        c_node.chars = parent.chars 
+        return c_node 
+    
+    queue = [perm_node(s2)]
+    
+    while queue:
+        
+        node = queue.pop()
+        
+        #rewards
+        if node.s == s1:
+            return True 
+        
+        # first breatdth 
+        for i in range(len(node.chars)):
+            new_node = clone(node)
+            new_node.s += new_node.chars[i]
+            new_node.chars = new_node.chars[0:i] + new_node.chars[i + 1 : :]
+            queue.append(new_node)
+    
+    return False 
+
+arr = ["mister","stimer"]
+print('{}'.format(is_permutation(arr[0], arr[1])))
+
+arr = ["mister","sister"]
+print('{}'.format(is_permutation(arr[0], arr[1])))
+
+def all_perms(s1):
+    class perm_node:
+        def __init__(self, raw = ""):
+            self.s = ""
+            self.chars = raw
+
+    def clone(parent):
+        c_node = perm_node()
+        c_node.s = parent.s  
+        c_node.chars = parent.chars 
+        return c_node 
+    
+    queue = [perm_node(s1)]
+    results = []
+
+    while queue:
+
+        node = queue.pop()
+
+        if len(node.s) == len(s1):
+            results.append(node.s)
+        
+        for i in range(len(node.chars)):
+            new_node = clone(node)
+            new_node.s += node.chars[i]
+            new_node.chars = node.chars[0 : i] + node.chars[i+1 : :]
+            queue.append(new_node)
+
+    return results 
+
+s = "team"
+print('{}'.format(all_perms(s)))
+
+def is_pangram(s: str):
+    s = s.lower()
+    histo = {} 
+    count = 0
+    
+    for i in range(len(s)):
+        c = s[i]
+
+        if ord(c) >= 97 and ord(c) <= 122:
+            if c not in histo:
+                histo[c] = True 
+                count += 1
+    return count == 26
+
+s1 = "How quickly daft jumping zebras vex!"
+print('{}'.format(is_pangram(s1)))
+
+s2 = "abcdef ghijkl mno pqrs tuv wxy, not so fast!"
+print('{}'.format(is_pangram(s2)))
+
+def is_perfect_pangram(s):
+    s = s.lower()
+    histo_lower = {}
+    histo_upper = {} 
+    count_lower = 0
+    count_upper = 0
+
+    for i in range(len(s)):
+        c = s[i]
+
+        if ord(c) >= 65 and ord(c) <= 90:
+            if c not in histo_upper:
+                histo_upper[c] = True 
+                count_upper += 1
+            else:
+                return False 
+            
+        if ord(c) >= 97 and ord(c) <= 122:
+            if c not in histo_lower:
+                histo_lower[c] = True
+                count_lower += 1
+            else:
+                return False 
+            
+
+    return  (count_upper + count_lower) == 26
+
+s_1 = "Playing jazz vibe chords quickly excites my wife."
+print('{}'.format(is_perfect_pangram(s_1)))
+
+s_2 = "Mr. Jock, TV quiz PhD, bags few lynx."
+print('{}'.format(is_perfect_pangram(s_2)))
+
+
+def best_time_buy_sell_stock(arr):
+    class stock_node:
+        def __init__(self, prices = []):
+            self.dest = prices 
+            self.path = []
+            self.prev = []
+
+    def clone(parent):
+        c_node = stock_node()
+        c_node.dest = parent.dest + []
+        c_node.path = parent.path + []
+        c_node.prev = []
+        return c_node 
+    
+    def is_profitable(node):
+        arr_ = node.path 
+        
+        if len(arr_) < 2:
+            return False 
+        
+        # increasing sequence
+        for i in range(len(arr_) - 1 ):
+            if arr_[i] > arr_[i + 1 ]:
+                return False 
+        return True 
+
+    queue = []
+    max_profit = 0 
+    
+    # init parent nodes (i.e. paths)
+    for i in range(len(arr)):
+        decision_paths = arr[i : :]
+        queue.append(stock_node(decision_paths))
+        
+    while queue:
+        node = queue.pop(0)
+
+        if len(node.path) > 1:
+            profit = node.path[-1] - node.path[0] 
+            if profit > max_profit :
+                max_profit = profit 
+        
+
+        if node.dest:
+
+            if not is_profitable(node):
+                while len(node.path) > 1:
+                    node.path.pop(0)
+
+            next_price_in_step = node.dest.pop(0) # remove current dest
+            new_node = clone(node) 
+            new_node.path += [next_price_in_step]
+            new_node.prev += [node] #
+            queue.append(new_node)
+    
+    return 'MAX PROFIT {}'.format(max_profit )
+
+arr = [6,4,6,5,9,7,6,12,2,6,11,2,4]
+print('{}'.format(best_time_buy_sell_stock(arr)))
+
+
+
+def best_time_buy_sell_stock2(arr):
+    class stock_node:
+        def __init__(self, prices = []):
+            self.dest = prices 
+            self.path = []
+            self.prev = []
+            self.pos = 0
+            self.end = len(arr)
+            self.hold = False 
+            self.trans = []
+
+    def clone(parent):
+        c_node = stock_node()
+        c_node.dest = parent.dest + []
+        c_node.path = parent.path + []
+        c_node.prev = []
+        c_node.pos = parent.pos
+        c_node.end = parent.end 
+        return c_node 
+  
+    
+    def is_profitable(node):
+        arr_ = node.path 
+        
+        # increasing sequence
+        for i in range(len(arr_) - 1 ):
+            if arr_[i] > arr_[i + 1 ]:
+                return False 
+        return True 
+    
+    def add_valid_trans(node, buffer):
+        great_buy = []
+        
+        for i in range(len(node.path)):
+            num = node.path[i]
+            great_buy.insert(i, num)
+        
+        # remove if in buffer 
+        tuple_old =  tuple( great_buy[0: len(great_buy) - 1 ])
+        i = 0
+        while i <  len(buffer) :
+            if buffer[i] ==  tuple_old:
+                buffer.pop(i)
+            else:
+                i += 1
+        buffer.append(tuple(great_buy))
+
+    queue = []
+    possible_profits = []
+    
+    # init parent nodes (i.e. paths)
+    queue.append(stock_node(arr))
+
+    while queue:
+
+        node = queue.pop(0)
+
+        if len(node.path) > 1:
+            
+            # trades are profittable 
+            if is_profitable(node):
+                add_valid_trans(node, possible_profits)
+            else:
+                # start trading from previous lost  
+                while len(node.path) != 1:
+                    node.path.pop(0)
+
+        if node.dest:
+            next_price_in_step = node.dest.pop(0) # remove current dest
+            new_node = clone(node) 
+            new_node.path += [next_price_in_step]
+            queue.append(new_node)
+    
+    return  '{} : {}'.format('MAXIMAL PROFIT:' , sum(list(map( lambda tup : tup[-1] - tup[0] , possible_profits ))))
+
+arr = [6,4,6,5,9,7,6,12,2,6,11,2,4]
+print('{}'.format(best_time_buy_sell_stock2(arr)))
+
+# forth tier included by adding k argument to solution 
+def best_time_buy_sell_stock3(arr, k):
+    class stock_node:
+        def __init__(self, prices = []):
+            self.dest = prices 
+            self.path = []
+            self.prev = []
+            self.pos = 0
+            self.end = len(arr)
+            self.hold = False 
+            self.trans = []
+
+    def clone(parent):
+        c_node = stock_node()
+        c_node.dest = parent.dest + []
+        c_node.path = parent.path + []
+        c_node.prev = []
+        c_node.pos = parent.pos
+        c_node.end = parent.end 
+        c_node.trans = parent.trans + []
+        return c_node 
+
+    def is_profitable(node):
+        arr_ = node.path 
+        
+        # increasing sequence
+        for i in range(len(arr_) - 1 ):
+            if arr_[i] > arr_[i + 1 ]:
+                return False 
+        return True 
+    
+    def add_trans(node):
+        arr_ = []
+        
+        for ele in node.path:
+            arr_.append(ele)
+        
+        node.trans.append( tuple(arr_) )
+
+    def calculate_profit(node, profit):
+        profit_ = 0
+        print(node.trans)
+        for tuple_ in node.trans:
+            
+            profit_ +=  tuple_[-1] - tuple_[0]
+
+        if profit < profit_:
+            return profit_
+        else:
+            return profit 
+
+    max_profit = 0
+    queue = []
+
+    for i in range(1):
+        queue.append(stock_node( arr[i : :] ))
+
+    while queue:
+        
+        node = queue.pop(0)
+
+        if len(node.trans) == k:
+            max_profit = calculate_profit(node, max_profit)
+
+        if node.dest:
+
+            if len(node.path) > 1 :
+
+                if is_profitable(node) :
+                    add_trans(node)                 
+
+                    # go back in graph prior to trans 
+                    new_node = clone(node)
+
+                    # clear trans from path on current node (this kicks off a second search after valid transaction)
+                    node.path = []
+
+                    # remove most recent path trans 
+                    new_node.trans.pop() 
+
+                    # remove previous nodes until to reach parent 
+                    while len(new_node.path) > 1:
+                        new_node.path.pop()
+                    
+                    # q
+                    queue.append(new_node)
+
+
+                else:
+                    # remove nodes starting from head 
+                    while len(node.path) > 1 :
+                        node.path.pop(0)
+
+            val = node.dest.pop(0)
+            new_node = clone(node)
+            new_node.path += [val]
+            queue.append(new_node)
+
+    return max_profit
+
+    
+arr = [6,4,6,5,9,7,6,12,2,6,11,2,4]
+print('{}'.format(best_time_buy_sell_stock3(arr, 2)))
+
+
+def str_loosely_interleave(s1 = "", s2 = "",  target_string = ""):
+    
+    class str_node :
+        def __init__ (self, s = ""):
+            self.current_string = s
+            self.arr_strings = [s1,s2]
+            self.col = 0
+
+    def clone(parent):
+        c_node = str_node()
+        c_node.current_string = parent.current_string 
+        c_node.arr_strings = parent.arr_strings + []
+        c_node.col = parent.col
+        return c_node
+    
+    queue = [str_node()]
+
+    while queue:
+        
+        node = queue.pop(0)
+
+        if target_string == node.current_string:
+            return True 
+        
+        if node.arr_strings[0] or node.arr_strings[1]:
+            new_node = clone(node)
+
+            if new_node.arr_strings[new_node.col] :
+                new_node.current_string += new_node.arr_strings[node.col][0]
+                new_node.arr_strings[node.col] = new_node.arr_strings[node.col][ 1 : : ]
+            
+            new_node.col = (new_node.col + 1) % 2
+            
+            queue.append(new_node)
+        
+    return False 
+
+arr =  ["dne","ail","daniel"]
+print('{}'.format(str_loosely_interleave( arr[0] , arr[1] , arr[2]) ))
+arr =  ["dne","ail","dalein"]
+print('{}'.format(str_loosely_interleave( arr[0] , arr[1] , arr[2]) ))
+arr =  ["dne","ail","ddaanneeiill"]
+print('{}'.format(str_loosely_interleave( arr[0] , arr[1] , arr[2]) ))
