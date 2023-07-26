aab0ad6175ec2c6cac47381691d738b16abdad6c
Hector Williams <hectorwilliams@Hectors-Air.lan>
2023-07-26 11:41:38 -0400

    trie -> size -> next -> auto_complete

--- chapter16/python/chap_16.py
+++ chapter16/python/chap_16.py
@@ -170,6 +170,111 @@ class Trie:
                         q.append(new_node)
                         break 
 
+    def size(self):
+        count = 0 
+        
+        if self.root == None:
+            return None 
+
+        q = [self.root]
+        
+        while q:
+            node = q.pop(0)
+            count += bool(node)
+
+            for c in node.children:
+                q.append(c)
+
+        return count 
+    
+    def next(self, target_s):
+        class s_node:
+            def __init__(self, parent):
+                self.depth = 0
+                self.trie_node = parent 
+
+        if self.root == None:
+            return None 
+
+        q = [s_node(self.root)]
+
+        while q:
+            node = q.pop(0)
+
+            if node.trie_node.s == target_s:
+                return target_s
+            else:
+                
+                match_child_trie_found = None 
+
+                for child_trie_node in node.trie_node.children:
+                    if target_s[0: node.depth + 1] == child_trie_node.s[0 : node.depth + 1]:
+                        match_child_trie_found = child_trie_node
+                
+                if match_child_trie_found:
+                    new_node = s_node(match_child_trie_found)
+                    new_node.depth = node.depth + 1
+                    q.append(new_node)
+                else:
+                    # no match found when comparing substrings
+                    if node.trie_node.children:
+                        return node.trie_node.children[0].s
+                    else:
+                        return node.trie_node.s 
+        
+    def auto_complete(self, target_s):
+
+        class s_node:
+            def __init__(self, parent):
+                self.depth = 0
+                self.trie_node = parent 
+        
+        def get_child_strings(arr_children):
+            result = [] 
+
+            q  = arr_children + [] 
+
+            while q:
+                trie_node = q.pop(0)
+                result.append(trie_node.s)
+
+                for child_trie_node in trie_node.children:
+                    q.append(child_trie_node)
+
+            return result 
+        
+        if self.root == None:
+            return None 
+        
+        q= [s_node(self.root)]
+
+        while q:
+            node = q.pop(0) 
+
+            print(node.trie_node.s)
+
+            if node.trie_node.s == target_s:
+                
+                # print remaining strings
+                return get_child_strings (node.trie_node.children)
+            
+            else:
+
+                match_child_trie_found = None 
+
+                for child_trie_node in node.trie_node.children:
+                    if child_trie_node.s[0 : node.depth + 1] == target_s[0 : node.depth + 1]:
+                        match_child_trie_found = child_trie_node 
+                
+                if match_child_trie_found:
+                    new_node = s_node(match_child_trie_found)
+                    new_node.depth = node.depth + 1
+                    q.append(new_node)
+                else:
+                    return get_child_strings (node.trie_node.children)
+
+
+
 
 trie_inst = Trie()
 trie_inst.add('a')
@@ -188,3 +293,6 @@ print(trie_inst.root.children[0].s)
 
 trie_inst.remove('ARF')
 trie_inst.last()
+print(trie_inst.size())
+print(trie_inst.next('arz'))
+print(trie_inst.auto_complete('ar'))
