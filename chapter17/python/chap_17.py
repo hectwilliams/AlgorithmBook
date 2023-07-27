40d4406299075fa02cf32dad14ee50f6326957a9
Hector Williams <hectwilliams@gmail.com>
2023-07-27 14:09:00 -0400

    AMGraph and removed old solution deck

--- chapter17/python/chap_17.py
+++ chapter17/python/chap_17.py
@@ -1,5 +1,8 @@
 # Edge List Implementation 
 
+from ctypes import Structure
+
+
 class ELGraph:
     def __init__(self):
         self.vertices = []
@@ -148,3 +151,150 @@ el_graph.add_edge(node1, node2, 0.50)
 print(el_graph.get_edge_value(node1, node2))    
 print(el_graph.set_edge_value(node1, node2, 10))     
 print(el_graph.get_edge_value(node1, node2))    
+
+
+# Adjacency Map 
+
+# map structure
+
+
+# [
+    
+#     [
+#         [0, 'A'],             # id , value
+        
+#         [[0, 0], [1, 0]] ,    # [to_id, edge value]
+    
+#     ], 
+        
+#     [
+#             [1, 'B'], 
+            
+#             [[0, 0], [1, 0]]
+                                      
+#     ]
+    
+# ]
+
+class AMGraph:
+    def __init__(self):
+        self.map = [] 
+
+    def add_vertex(self, value):
+        idx = -1 
+
+        for i in range(len(self.map) - 1):
+            current_id = self.map[i][0]
+            next_id = self.map[i + 1][0]
+
+            if current_id + 1 != next_id:
+                idx = current_id + 1
+                break 
+        
+        if idx == -1:
+            idx = len(self.map)    
+
+        self.map.append( [ [idx, value],  [] ] )  
+
+        for i in range(len(self.map)):
+            target_node_id = self.map[i][0][0]
+            self.map[-1][1].append([target_node_id, 0]) # [  [id1, value], [ [id0, w10], [id1, w11 ]  ] 
+            
+            # check previous nodes, weight or zero weight value connection to new added node is required 
+
+            for k in range(len(self.map) - 1):
+                idx_ = -1 
+                for map_info in self.map[k][1]:
+                    if map_info[0] == idx:
+                        idx_ = idx
+                if idx_ == -1:
+                    self.map[k][1].append( [ idx, 0] )
+        return idx 
+
+    def remove_vertex(self, node_id):
+        bool_state = False  
+      # remove node 
+        for i in range(len(self.map)):
+            if self.map[i][0][0] == node_id:
+                self.map.pop(i)
+                bool_state = True 
+                break
+
+     # remove connections on other node 
+        
+        for i in range(len(self.map)):
+            
+            arr = self.map[i][1]
+
+            for k in  range(len(arr)):
+                if node_id  == arr[k][0]:
+                    arr.pop(k)
+                    break 
+        
+        return bool_state 
+    
+    def get_vertex_value(self, id):
+        for node_map_section in self.map:
+            if node_map_section[0][0] == id:
+                return node_map_section[0][1]
+   
+        return False 
+    
+    def set_vertex_value(self, id, value):
+        for node_map_section in self.map:
+            if node_map_section[0][0] == id:
+                node_map_section[0][1] = value
+                return True 
+        return False 
+
+    def add_edge (self, id1, id2, value):
+        for node_map_section in self.map:
+            for rx_node in node_map_section[1]:
+                if node_map_section[0][0] == id1 and rx_node[0] == id2:
+                    rx_node[1] = value 
+                    return True 
+        return  False 
+    
+    def remove_edges (self, node_id):
+        count_removes = 0 
+        for node_map_section in self.map:
+            for rx_node in node_map_section[1]:
+                if node_map_section[0][0] == node_id or rx_node[0] == node_id:
+                    rx_node[1] = 0
+                    count_removes += 1
+        return count_removes > 0 
+    
+    def remove_edge(self, node1_id, node2_id):
+        for node_map_section in self.map:
+            for rx_node in node_map_section[1]:
+                if node_map_section[0][0] == node1_id or rx_node[0] == node2_id:
+                    rx_node[1] = 0    
+    
+    def adjacent (self, node1_id, node2_id):
+        for node_map_section in self.map:
+            for rx_node in node_map_section[1]:
+                if node_map_section[0][0] == node1_id or rx_node[0] == node2_id:
+                    return True 
+        return False 
+    
+    def neighbors (self, node_id):
+        result = [] 
+        for node_map_section in self.map:
+                if node_map_section[0][0] == node_id:
+                    for neighbor_info in node_map_section[1] :
+                        if neighbor_info[0] != node_id:
+                            result.append(neighbor_info[0])
+        return result 
+    
+
+am_graph = AMGraph()
+node1 = am_graph.add_vertex('A')
+node2 = am_graph.add_vertex('B')
+
+# am_graph.remove_vertex(node1)
+# am_graph.remove_vertex(node1)
+print(  am_graph.get_vertex_value(node1) )
+am_graph.set_vertex_value(node1, 'T')
+am_graph.add_edge(node1, node2, 22)
+print(am_graph.map)
+print(am_graph.neighbors(node1))
