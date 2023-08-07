6dac8deb4a24b974fc2a0736fc2b034dc6c9abbc
Hector Williams <hectwilliams@gmail.com>
2023-08-07 19:54:43 -0400

    AVL remove with .balance class variable

--- chapter19/python/chap_19.py
+++ chapter19/python/chap_19.py
@@ -23,7 +23,7 @@ class AVL_NODE:
 
         while queue:
             node = queue.pop(0)
-
+            print(node.tree_node.val, 'search', node.tree_node.left)
             if node.depth > result_height:
                  result_height = node.depth 
 
@@ -38,8 +38,7 @@ class AVL_NODE:
         return result_height 
     
     def is_balanced(self):
-        left  = 0 
-        right = 0 
+        return abs(self.balance) <= 1
         
 class AVL_TREE:
     def __init__(self):
@@ -88,7 +87,117 @@ class AVL_TREE:
                 node = path.pop() 
                 node.balance += balance_update 
 
+    def remove(self, value):
+        nodes = [] 
+        node = None 
+
+        if self.head == None:
+            return 
+        
+        node = self.head 
+
+        while node:
+            
+            if value == node.val:
+
+                if len(nodes) == 0:
+                    grand_child = None 
+                    child_r = node.right 
+                    child_l = node.left 
+
+                    if child_r:
+                        grand_child = child_r.left 
+                        child_r.left = child_l or grand_child
+
+                    if child_l:
+                        curr_node = child_l
+                        while curr_node.right:
+                            curr_node = curr_node.right 
+                        curr_node.right = grand_child 
+
+                    self.head = child_r 
+                
+                elif nodes[-1].right == node : 
+                    grand_child = None 
+                    child_r = node.right 
+                    child_l = node.left 
+
+                    if child_r :
+                        grand_child = child_r.left 
+                        child_r.left = child_l or grand_child
+                    
+                    if child_l:
+                        curr_node = child_l
+                        while curr_node.right:
+                            curr_node = curr_node.right 
+                        curr_node.right = grand_child 
+                    
+                    nodes[-1].right = child_r 
+
+                elif nodes[-1].left == node:
+                    grand_child = None 
+                    child_r = node.right 
+                    child_l = node.left 
+
+                    if child_l:
+                        grand_child = child_l.right 
+                        child_l.right = child_r or grand_child
+
+                    if child_r:
+                        curr_node = child_r 
+                        while curr_node.left:
+                            curr_node = curr_node.left 
+                        curr_node.left = grand_child
+                    
+                    nodes[-1].left = child_l 
+            
+
+                # update balance
+                nodes.extend( [child_l, child_r] ) 
+
+                while nodes:
+
+                    curr = nodes.pop() 
+                    
+                    left = curr.left 
+                    if left:
+                        left = left.height()  + 1
+                    else:
+                        left = 0 
+                         
+                    right = curr.right
+                    if right:
+                        right = -right.height()  -1
+                    else: 
+                        right = 0 
+
+                    curr.balance = left + right  
+                    
+                break 
+
+            
+            nodes.append(node)
+            
+            if value < node.val:
+                node = node.left 
+            
+            elif value > node.val :
+                node = node.right 
+            
+            else :
+                node = None  
+        
+
+
 tree = AVL_TREE()
 tree.add(40)
 tree.add(22)
 tree.add(62)
+tree.add(70)
+tree.add(32)
+tree.add(21)
+
+tree.remove(40)
+print(tree.head.val, tree.head.balance)
+print(tree.head.left.val, tree.head.left.balance)
+print(tree.head.left.left.val, tree.head.left.left.balance)
