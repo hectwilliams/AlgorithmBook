96db9c1561e2dbb57fe70aacce033d95bc495f33
Hector Williams <hectwilliams@gmail.com>
2023-08-22 16:30:33 -0400

    regex

--- review/java_fun_child/io/wr/FileWritingExample.java
+++ review/java_fun_child/io/wr/FileWritingExample.java
@@ -8,13 +8,16 @@ import java.io.IOException;
 
 import java.nio.file.Paths;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class FileWritingExample {
     public static void main(String[] args) {
 
         String currDir = Paths.get("review", "java_fun_child", "io", "wr").toAbsolutePath().normalize().toString();
         String filename = Paths.get(currDir, "example.txt").toString() ;  
-
-        String message = "Hey Tarzan, my number is 676-313-9383";
+        String message = "Hey Tarzan, my number is 676-313-9383";   
         
         try {
             
@@ -34,6 +37,13 @@ public class FileWritingExample {
         } catch (IOException e) {
             System.out.println("An error occurred while writing the file");
             e.printStackTrace();
+        } finally {
+            Pattern pattern = Pattern.compile("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(message);
+            if (matcher.find()) {
+                // print telephone number 
+                System.out.println(  message.substring( matcher.start(), matcher.end() - 1 ) );
+            }
         }
     }
 }
