edcb281054592d023d0bafd0761ae50573eeac2f
HW <hectwilliams@gmail.com>
2026-07-28 10:21:37 -0400

    added debug blocks (toggle on/off)

--- leetcode/4Sum/script.sh
+++ leetcode/4Sum/script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#
+# Example: ./script 4 
+# "Runs the 4th test in program"
+#
+#
+
+if (( $#  != 1 )) ; then 
+    printf "Error! Arugument required.\nTest Options:\n1: Test 1\n2: Test 2\n3: Test 3\n4: Test 4\n"
+    exit 1
+fi
+
 g++ Solution.cpp -std=c++17   -fsyntax-only -Wall -Wextra -Wpedantic   &&  g++  -std=c++17  Solution.cpp  -o run  ; 
 
 ./run "$1"
