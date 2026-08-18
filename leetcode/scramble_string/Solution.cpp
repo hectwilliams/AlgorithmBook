0080a642763ee23dad0393aa580975ad581c31c5
HW <hectwilliams@gmail.com>
2026-08-17 20:14:07 -0400

    updated solution

--- leetcode/scramble_string/Solution.cpp
+++ leetcode/scramble_string/Solution.cpp
@@ -10,53 +10,42 @@
 #include <stdexcept> // Required for standard exceptions
 #include <string>
 
-struct Node; 
+struct Node2; 
 
-using Q = std::deque<Node *>;
+using Q2 = std::deque<Node2 *>;
+
 using UsedList = std::map<std::string, bool>;
 using STable = std::map<std::size_t, std::string> ;
 using Words = std::vector< std::string>;
 
-struct Node {
+struct Node2 {
     Words data;
-    std::string acc; 
+    Words result;
+    // std::string acc; 
     int index;
 };
 
-
 std::ostream& operator<<(std::ostream& os, const Words & data) {
-
-     os << "[ ";
+    os << "[ ";
     for ( const auto &value: data ) {
         os << " " << value << ", "  ;
     }
      os << " ]\n";
-
     return os; 
-
 }
 
 
-std::ostream& operator<<(std::ostream& os, Node* node) {
-
-
+std::ostream& operator<<(std::ostream& os, Node2* node) {
      os << "[\n";
-    
     //  os  << "\ts1: "<< node->s1 << " s2: " << node->s2  << "\n";
-
      for (const auto &value: node->data) {
         os <<  value <<  ",";
      }
      os << "\n]\n";
 
     return os; 
-
 }
 
-
-void add_to_queue() {
-
-}
 
 bool histo_valid (std::string s1, std::string s2) {
     
@@ -84,11 +73,10 @@ bool histo_valid (std::string s1, std::string s2) {
     return (sum == 0);
 }
 
-
-void init_split(std::string s1, Q &q) {
+void init_split2(std::string s1, Q2 &q) {
     // add s1 string to node 
 
-    Node *node = new Node{ {}, "", 0};
+    Node2 *node = new Node2{ {}, {},  0};
 
     node->data.push_back(s1);
 
@@ -97,12 +85,21 @@ void init_split(std::string s1, Q &q) {
 }
 
 
-bool search_2_test(Node *node, std::string s) {
+bool search_2_test2(Node2 *node, std::string s) {
     
-    
+    if (node->result.size() != s.length()) {
+        
+        return false ;
+
+    }
+
     for (std::size_t i = 0; i < s.length(); i++) {
         
-        if ( s[i] != node->acc[i] ) {
+        if ( node->result[i].length()  !=  1 ) {
+            return false;
+        }
+        
+        if ( std::string(1, s[i] ) != node->result[i] ) {
             return false; 
         }
 
@@ -112,146 +109,168 @@ bool search_2_test(Node *node, std::string s) {
 
 }
 
-void search_2(std::string s, Node *node, Q & q ) {
+
+void search_3(std::string s, Node2 *node, Q2 & q ) {
 
     char target_c = s[node->index];
+    
+    std::cout << " INDEX TARGET: " << node->index << "\n";
+    std::cout << " NEXT LENGTH TARGET: " << node->index + 1 + 1 << "\n";
+
+    std::cout << " DATA TARGET: " << node->data << "\n";
+
+    // if (node->index == 11) {
+
+    //     std::cout << node->data;
+
+    //     std::cout << node->result;
+
+    // //     std::cout << node->result;
+
+    // //     std::cout << " x-------x: " << "\n";
+
+    //     assert(0);
+    // }
 
     std::string analysis_s = node->data[0];
-    std::cout  << analysis_s << "\n";
 
-    std::cout << " ANALYSIS\t" << analysis_s;
-    std::cout << " ANALYSIS DONE" <<  "\n";
-    std::cout << " AC "  << node->acc  << "\n";
-    std::cout << " TARGET "  << target_c  << "\n";
+
 
     for (int i = 0; i < analysis_s.length(); i++) {
-        
 
         if ( target_c == analysis_s[i] ) {
+            
+            if (i == 0) { 
 
-            if (i == 0) {
+                Node2 *new_node = new Node2{ {}, node->result, node->index }; // copy 
 
-                // split 
+                      // split 
                 std::string left = analysis_s.substr(0, 1);
                 std::string right = analysis_s.substr(1);
 
-                Node *new_node = new Node{ {}, node->acc , node->index }; // copy 
+                // std::cout << left << "\n";
+                // std::cout << right << "\n" ;
                 
                 // remove prev head 
                 Words next_vector = Words( node->data.begin() + 1 , node->data.end() );
                 
-                // new head 
-                Words left_vector;
-                
+                Words right_vector;
                 if (right.length()) {
-                    left_vector.push_back(right);
+                    right_vector.push_back(right);
                 }
-
-                // concat
-                left_vector.insert(left_vector.end(), next_vector.begin(), next_vector.end());
+                
+                Words collection;
+                
+                collection.insert(collection.end(), right_vector.begin(), right_vector.end());
+                collection.insert(collection.end(), next_vector.begin(), next_vector.end());
                 
                 // write to new node 
-                new_node->data = left_vector;
-
-                // append char to accumulator
-                new_node->acc += left;
+                new_node->result.push_back(left);
+                new_node->data = collection;
 
                 // incr
                 new_node->index = node->index + 1;
 
-                std::cout << new_node->acc;
+                //
                 std::cout << new_node->data;
+                std::cout << new_node->result;
+
+                // 
                 std::cout << "------0" << "\n";
 
                 q.push_back(new_node);
 
-            } else if (i == analysis_s.length()-1) {
 
-                // split 
+            } else if (i == analysis_s.length() - 1) {
+                               // split 
                 std::string left = analysis_s.substr(0, analysis_s.length() - 1 );
                 std::string right = analysis_s.substr( analysis_s.length() - 1 );
 
                 // remove prev head 
                 Words next_vector = Words( node->data.begin() + 1 , node->data.end() ); 
 
-                Node *new_node = new Node{ {}, node->acc , node->index }; // copy 
+                Node2 *new_node = new Node2{ {}, node->result , node->index }; // copy 
                 
                 // new head 
-
-                Words left_vector;
-
-                if (left.length()) {
-                    left_vector.push_back(left);
-                }
-
+                Words left_vector = {left};
+                Words right_vector = {right};
+                Words collection;
                 // concat 
-                left_vector.insert(left_vector.end(), next_vector.begin() , next_vector.end());
+                // collection.insert(collection.end(), right_vector.begin(), right_vector.end());
+                collection.insert(collection.end(), left_vector.begin() , left_vector.end());
+                collection.insert(collection.end(), next_vector.begin() , next_vector.end());
 
                 // set data 
-                new_node->data = left_vector;
-
-                // set accum
-                new_node->acc += right;
+                new_node->data = collection;
 
                 new_node->index = node->index + 1;
 
-                std::cout << new_node->acc;
+                new_node->result.push_back(right);
+
                 std::cout << new_node->data;
+                std::cout << new_node->result;
                 std::cout << "------1" << "\n";
 
                 q.push_back(new_node);
 
             } else {
                 
-                // split 
-                std::string left = analysis_s.substr(0, i + 1 );
-                std::string right = analysis_s.substr(i + 1 );
-                std::string uni = left.substr(left.length() - 1);
-                left = left.substr( 0, left.length() - 1);
-                
-                Node *new_node = new Node{ {}, node->acc , node->index }; // copy 
-
+                // split (overload left)
+                Node2 *new_node1 = new Node2{ {}, node->result,  node->index }; // copy 
+                std::string left = analysis_s.substr( 0, i + 1 );
+                std::string right = analysis_s.substr( i + 1 );
+                // std::string uni = left.substr( left.length() - 1 );
+                // left = left.substr( 0, left.length() - 1 );
                 Words next_vector = Words( node->data.begin() + 1 , node->data.end() ); 
 
                 // new head 
                 Words left_vector = {left};
                 Words right_vector = {right};
-                Words collection = {};
-
-                // concat with left
-                // collection.insert(collection.end(), left_vector.begin(), left_vector.end());
-
-                // concat with right
-                left_vector.insert(left_vector.end(), right_vector.begin(), right_vector.end());
-
-                left_vector.insert(left_vector.end(), next_vector.begin(), next_vector.end());
-
-
-                // set data 
-                new_node->data = left_vector;
-
-                // set accum 
-                new_node->acc += uni;
-
-                new_node->index =  node->index + 1;
+                Words collection;
                 
-                std::cout << new_node->acc;
-                std::cout << new_node->data;
-                std::cout << "------" << "\n";
-                q.push_back(new_node);
+                collection.insert(collection.end(), left_vector.begin(), left_vector.end());
+                collection.insert(collection.end(), right_vector.begin(), right_vector.end());
+                collection.insert(collection.end(), next_vector.begin(), next_vector.end());
                 
+                new_node1->data = collection;
+                q.push_back(new_node1);
+                
+                // split (overload right)
+                Node2 *new_node2 = new Node2{ {}, node->result,  node->index }; // copy 
+                left = analysis_s.substr( 0, i  );
+                right = analysis_s.substr( i );
+                next_vector = Words( node->data.begin() + 1 , node->data.end() ); 
+                left_vector = {left};
+                right_vector = {right};
+                collection.clear();
+                
+                //swapp
+                collection.insert(collection.end(), right_vector.begin(), right_vector.end());
+                collection.insert(collection.end(), left_vector.begin(), left_vector.end());
+
+                collection.insert(collection.end(), next_vector.begin(), next_vector.end());
+                
+                new_node2->data = collection;
+
+                q.push_back(new_node2);
+
+                std::cout << new_node1 << "\n";
+                std::cout << new_node2 << "\n";
+
             }
         }
 
     }
-
+        
 }
 
 class Solution {
 public:
     bool isScramble(std::string s1, std::string s2) {
         UsedList usedList; 
-        Q q;
+        // Q q;
+
+        Q2 q;
 
         if (s2.length() == 0) {
             
@@ -259,7 +278,6 @@ public:
                 return true;
             else 
                 return false; 
-
         }
 
         if ( s2.length() == 1 ) {
@@ -280,25 +298,27 @@ public:
             return false; 
         }
 
-        // q.push_back(new Node{  STable{{0, s1 }},  0});
-        
         // init
-        init_split(s1, q);
+        init_split2(s1, q);
 
         bool found_u = false;
 
         while (!q.empty())  {
              
-            Node *node = q.front();
+            Node2 *node = q.front();
 
             q.pop_front(); 
-
-            if (node->acc.length() == s2.length()) {
+            
+            if (s2.length() == node->result.size()) {
+                std::cout << " LENS " << s1.length() << " " << node->result.size() << "\n"; 
+                std::cout << " result "<< node->result << "\n";
                 
-                found_u |= true;
-
+                if (search_2_test2(node, s2))
+                    found_u |= true;
+                
             } else {
-                search_2(s2, node, q);
+
+                search_3(s2, node, q);
             }
             
             // if (node->index == s1.length()) {
@@ -361,7 +381,13 @@ int main(int param_count, char *args[]) {
             case(4):
                 s1 = "abcde";
                 s2 = "caebd";
+                break; 
 
+            case(5):
+                s1 = "abcd";
+                s2 = "bcad";
+                break; 
+                
             default: 
                 break;
         
