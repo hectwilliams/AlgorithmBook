cd9f1ae6563f1679649d6edc5754a74fa4a6f7d9
HW <hectwilliams@gmail.com>
2026-09-10 04:47:47 -0400

    updated main.c

--- leetcode/largest_rectangle_histogram/main.c
+++ leetcode/largest_rectangle_histogram/main.c
@@ -99,6 +99,21 @@ int main(int param_count, char *args[]) {
             len = &test_vector_14_len;
             break;
 
+        case 15: 
+            heights = test_vector_15;
+            len = &test_vector_15_len;
+            break;
+
+        case 16: 
+            heights = test_vector_16;
+            len = &test_vector_16_len;
+            break;
+
+        case 17: 
+            heights = test_vector_17;
+            len = &test_vector_17_len;
+            break;
+
         default:
             break;
         
