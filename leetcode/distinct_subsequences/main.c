7cbf4f32d501d2b4f68e12ad418b6c30e32bb947
HW <hectwilliams@gmail.com>
2026-09-23 10:05:13 -0400

    new method

--- leetcode/distinct_subsequences/main.c
+++ leetcode/distinct_subsequences/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+#include <assert.h>
 
 #define test_01_s babgbag
 
@@ -12,6 +13,13 @@ typedef struct Node {
     int count;
     int index;
 } Node; 
+
+
+typedef struct TNode {
+    int index;
+    struct TNode *next; 
+} TNode; 
+
 
 int length(char *s) {
     int count = 0;
@@ -162,30 +170,183 @@ int search(Node * node, char *s, int s_length, char *t,  int t_length) {
 
 }
 
+void recursive_search(char *t, int t_length, char *s, int s_length, int *bitmask, int * n_sequences) {
+
+    
+    if (t_length == 0) {
+        
+        *n_sequences =  *n_sequences  + 1;
+    }
+    
+    else if (t_length == 1) {
+        // find char in s string (minding bitmask)
+        // TNode node;
+        // copy mask;
+        for (int i = 0; i < s_length; i++) {
+            if (bitmask[i] && t[0] == s[i]) {
+                recursive_search("", 0, s, s_length, bitmask, n_sequences); 
+            }
+        }
+
+    } else {
+        
+            TNode *front_ll = NULL;
+    TNode *front_head = NULL;
+
+    TNode *end_ll = NULL;
+    TNode *end_head = NULL;
+
+   
+        char front = t[0];
+        char end = t[t_length - 1];
+
+        for (int i = 0; i < s_length; i++) {
+
+            if (front == s[i]) {
+                TNode *tmp = malloc( sizeof(TNode));
+                tmp->index = i; 
+                tmp->next = NULL;
+                if (front_ll == NULL) {
+                    front_ll = tmp;
+                    front_head = tmp; 
+                } else {
+                    front_ll->next =  tmp;
+                    front_ll = tmp;
+                }
+                
+            }
+
+            if (end == s[i]) {
+                TNode *tmp = malloc( sizeof(TNode));
+                tmp->index = i; 
+                tmp->next = NULL;
+
+                if (end_ll == NULL) {
+                    end_ll = tmp;
+                    end_head = tmp; 
+                } else {
+                    end_ll->next =  tmp;
+                    end_ll = tmp;
+                }
+                    
+            }
+           
+        }
+
+        int n_samples_between = 0 ; 
+        TNode *w_front;
+        TNode *w_end = end_head;
+
+        while (w_end) {
+
+            w_front = front_head;
+
+            while (w_front) {
+
+                n_samples_between = w_end->index - w_front->index - 1;
+                
+                if (bitmask[w_end->index] || bitmask[w_front->index] ) {
+                    // noop
+                } else if (n_samples_between > 0) {
+
+                    printf(" %d - %d  = %d\n", w_end->index, w_front->index, n_samples_between);
+
+                    // create new update bitmask 
+                     int *new_bitmask = calloc( (unsigned)s_length,  sizeof(int));
+
+                     // copy argument bitmask
+                     memcpy(new_bitmask, bitmask, (unsigned) s_length); // valid slots for next value 
+
+                     // set mask valid window
+                     int k = w_front->index + 1;
+                     while (k < w_end->index) {
+                        new_bitmask[k++] = 1;
+                     }
+                     //  new_bitmask[w_end->index ] = 1;
+                     //  new_bitmask[w_front->index ] = 1;
+
+
+                     printf("%d %d %d %d %d %d %d\n", new_bitmask[0], new_bitmask[1],  new_bitmask[2], new_bitmask[3], new_bitmask[4], new_bitmask[5], new_bitmask[6]);
+
+                     // reduce t 
+                     int t_length_new = t_length - 2;
+                     char *t_new = calloc( t_length_new,  sizeof(int));
+                     
+                     memcpy(t_new, t + 1, t_length_new );
+                     printf("new T %s\n", t_new);
+
+                     recursive_search(t_new, t_length_new, s, s_length, new_bitmask, n_sequences); 
+
+                     printf("new T %d \n", *n_sequences);
+
+                    // passes search analysis requirement 
+                }
+
+                w_front = w_front->next;
+                
+            }
+            
+            w_end = w_end->next;
+        }
+
+        // print_ll(end_head);
+        // print_ll(front_head);
+        
+        
+    }
+}
+
+void print_ll(TNode *node) {
+
+    while (node) {
+        printf("[%d]", node->index);
+        node = node->next; 
+    }
+    printf("\n");
+}
+
 int numDistinct(char* s, char* t) {
 
-    // init node 
-    Node *node = malloc( sizeof(Node));
+    // // init node 
+    // Node *node = malloc( sizeof(Node));
+     
     
     int s_length = length(s);
     int t_length = length(t);
-    
+
     printf("%d\n", s_length);
     printf("%d\n", t_length);
     
-    if (s_length == -1) {
-        // null-termination node found in string 
-        return  0;
-    }
+    // if (s_length == -1) {
+    //     // null-termination node found in string 
+    //     return  0;
+    // }
     
-    if (strcmp(s, t) == 0) {
-        return 1;
+    // if (strcmp(s, t) == 0) {
+    //     return 1;
+    // }
+
+    // init_node(node, s, s_length);
+    // int ret = search(node, s, s_length, t, t_length);
+
+    // return ret;
+
+
+    int *bitmask = calloc( (unsigned)s_length,  sizeof(int));
+    int n_sequences = 0;
+
+    if (t_length == 1) {
+
+        for (int i = 0; i < s_length; i++) {
+            n_sequences += +(t[0] == s[i]);
+        }
+
+    } else if (t_length > 1) {
+
+        recursive_search(t, t_length, s, s_length, bitmask, &n_sequences);
+
     }
-
-    init_node(node, s, s_length);
-    int ret = search(node, s, s_length, t, t_length);
-
-    return ret;
+    return n_sequences; 
 
 }
 
